891a43a56414331493732d00dc2ad03c6f392fa7
Sara Szwejkowska <saraszw@op.pl>
2022-08-04 20:48:19 +0200

    created JS, part1

---
+++ main.js
@@ -0,0 +1,50 @@
+const fileInput = document.querySelector('.file-input'),
+	filterOptions = document.querySelectorAll('.filter button');
+filterName = document.querySelector('.filter-info .name');
+filterValue = document.querySelector('.filter-info .value');
+filterSlider = document.querySelector('.slider input');
+previewImg = document.querySelector('.preview-img img');
+chooseImgBtn = document.querySelector('.choose-img');
+
+let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
+
+
+const loadImage = () => {
+	let file = fileInput.files[0];
+	if (!file) return;
+	previewImg.src = URL.createObjectURL(file);
+	previewImg.addEventListener('load', () => {
+		document.querySelector('.container').classList.remove('disable');
+	});
+};
+
+filterOptions.forEach((option) => {
+	option.addEventListener('click', () => {
+		document.querySelector('.filter .active').classList.remove('active');
+		option.classList.add('active');
+		filterName.innerText = option.innerText;
+if(option.id ==="brightness") {
+    filterSlider.value = `${brightness}%`;
+    filterValue.innerText = `${brightness}%`;
+}
+
+	});
+});
+const updateFilter = () => {
+	filterValue.innerText = `${filterSlider.value}%`;
+    const selectedFilter = document.querySelector(".filter .active");
+
+    if(selectedFilter.id ==="brightness") {
+brightness = filterSlider.value;
+    } else if (selectedFilter.id === "saturation") {
+    saturation = filterSlider.value;
+    } else if (selectedFilter.id === "inversion") {
+    inversion = filterSlider.value;
+    } else {
+    grayscale = filterSlider.value;{
+    }
+}
+}
+fileInput.addEventListener('change', loadImage);
+filterSlider.addEventListener('input', updateFilter);
+chooseImgBtn.addEventListener('click', () => fileInput.click());
